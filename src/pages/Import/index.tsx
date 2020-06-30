cc1b9d658906e20c62cf94c3a6ea52db7fdd0bd2
lix.digito <lix.digito@hotmail.com>
2020-06-29 21:35:00 -0300

    implementação

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,30 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    
 
-    // TODO
+    uploadedFiles.map(async file => {
+      const data = new FormData();
 
-    try {
-      // await api.post('/transactions/import', data);
-    } catch (err) {
-      // console.log(err.response.error);
-    }
+      data.append('file', file.file, file.name );
+      try {
+        await api.post('/transactions/import', data);
+      } catch (err) {
+        console.log(err.response.error);
+      }
+    });
+    history.push('/');    
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+      const filesProps = files.map(file => {
+        return {
+          file,
+          name: file.name,
+          readableSize: filesize(file.size)
+        }
+      });
+      setUploadedFiles(filesProps);
   }
 
   return (
